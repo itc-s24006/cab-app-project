76acc1b479d02a8c3e131e37d1a5f18d45b71ce1
itc-s24006 <s24006@std.it-college.ac.jp>
2025-11-12 12:41:32 +0900

    プロジェクトにtailwindCSSとflowbiteインストール・ダークモードだけ動作確認済み

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,34 @@
+// "use client";
+
 import styles from "./page.module.css";
 import { getQuizList } from "@/lib/microcms";
+import { Button, DarkThemeToggle } from "flowbite-react";
 
 export default async function Home() {
   const quizList = await getQuizList();
+  const quizzes = quizList.contents.map((quiz, index) => ({
+    ...quiz,
+    q_number: index + 1, // 問題番号
+  }));
 
   return (
-    <div className={styles.page}>
-      <h1 className={styles.title}>
-        CAB【暗算】
-        <br />
-        練習
-      </h1>
-      <div>
-        {quizList.contents.map((quiz) => (
-          <p key={quiz.id}>{quiz.question}</p>
-        ))}
+    <main className="p-8 flex flex-col items-center gap-4">
+      <div className={styles.page}>
+        <h1 className={styles.title}>
+          CAB【暗算】
+          <br />
+          練習
+        </h1>
+        <div>
+          {quizzes.map((quiz) => (
+            <div key={quiz.id}>
+              <p>Q{quiz.q_number}:</p>
+              <p>{quiz.question}</p>
+            </div>
+          ))}
+        </div>
+        <button className={styles.button}>ボタン</button>
       </div>
-    </div>
+    </main>
   );
 }
